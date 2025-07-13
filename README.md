653bcdf07166fe79c0ebeb6b1e126e37c723b690
Adithya Nair <adtya@adtya.xyz>
2025-07-13 22:44:01 +0530

    some refactor

--- README.md
+++ README.md
@@ -31,3 +31,6 @@
 1. Firefox
 If the overlay is added as above, use `pkgs.firefox-overkill` instead of the typical `pkgs.firefox`. If the overlay is not added, use `inputs.recipes.packages.${pkgs.system}.firefox-overkill` instead.
 
+## Modules
+ coming soon ...
+
--- packages/firefox/default.nix
+++ packages/firefox/default.nix
@@ -14,12 +14,14 @@ let
     rev = "e66de491acd5e87f53abc3dfe9622f7ce5e2a101";
     hash = "sha256-Kkk3mqfQCXf/J+SoAKki46e4OoD6ZPHTlXT+yezj6gU=";
   };
+  lockPrefs = prefs: builtins.replaceStrings [ "user_pref(" ] [ "lockPref(" ] prefs;
+  loadPrefs = fileName: builtins.readFile "${betterfox-src}/${fileName}.js";
   extraPrefs = ''
+    ${lib.optionalString (!disableFastfox) (lockPrefs (loadPrefs "Fastfox"))}
+    ${lib.optionalString (!disablePeskyfox) (lockPrefs (loadPrefs "Peskyfox"))}
+    ${lib.optionalString (!disableSecurefox) (lockPrefs (loadPrefs "Securefox"))}
+    ${lib.optionalString (!disableSmoothfox) (lockPrefs (loadPrefs "Smoothfox"))}
     ${builtins.readFile ./prefs.cfg}
-    ${lib.optionalString (!disableFastfox) (builtins.readFile "${betterfox-src}/Fastfox.js")}
-    ${lib.optionalString (!disablePeskyfox) (builtins.readFile "${betterfox-src}/Peskyfox.js")}
-    ${lib.optionalString (!disableSecurefox) (builtins.readFile "${betterfox-src}/Securefox.js")}
-    ${lib.optionalString (!disableSmoothfox) (builtins.readFile "${betterfox-src}/Smoothfox.js")}
   '';
 
 in
