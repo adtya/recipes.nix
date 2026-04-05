dbbd05410871d679a84e0937b5d0a98ee5ffa919
Adithya Nair <adtya@adtya.xyz>
2026-04-06 00:54:52 +0530

    update desktop preset

--- modules/nixos/presets/desktop.nix
+++ modules/nixos/presets/desktop.nix
@@ -17,6 +17,22 @@ in
   };
 
   config = lib.mkIf cfg {
+    boot = {
+      bootspec.enable = true;
+      consoleLogLevel = 3;
+      initrd = {
+        systemd.enable = true;
+        verbose = false;
+      };
+      kernelParams = [ "quiet" ];
+      kernel.sysctl = {
+        "vm.dirty_ratio" = 3;
+      };
+      loader = {
+        timeout = 0;
+        efi.canTouchEfiVariables = true;
+      };
+    };
 
     environment.systemPackages = with pkgs; [ ghostty ];
 
