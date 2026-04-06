ac908a2699112784f9a23e7d64520c4aa44f6a85
Adithya Nair <adtya@adtya.xyz>
2026-04-06 23:34:56 +0530

    add devtools from home-manager

--- modules/nixos/misc/devtools.nix
+++ modules/nixos/misc/devtools.nix
@@ -7,6 +7,7 @@
 }:
 let
   cfg = config.xyz.adtya.recipes.misc.devtools;
+  user-cfg = config.xyz.adtya.recipes.core.users;
   inherit (pkgs.stdenv.hostPlatform) system;
 in
 {
@@ -17,16 +18,68 @@ in
         default = false;
         description = "Enable some dev tools";
       };
+      git = {
+        user-name = lib.mkOption {
+          type = lib.types.str;
+          default = user-cfg.primary.long-name;
+          defaultText = lib.literalMD "[config.xyz.adtya.recipes.core.users.primary.long-name](#xyzadtyarecipescoreusersprimarylong-name)";
+          description = "Name of git user";
+        };
+        user-email = lib.mkOption {
+          type = lib.types.str;
+          default = "";
+          description = "Email of git user";
+        };
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
+    environment.systemPackage = with pkgs; [
+      delta
+      doctl
+      flyctl
+      gh
+      hcloud
+      ripgrep
+
+    ];
     programs = {
       direnv = {
         enable = true;
         nix-direnv.enable = true;
       };
-      git.enable = true;
+      git = {
+        enable = true;
+        config = {
+          user = {
+            email = cfg.git.user-email;
+            name = cfg.git.user-name;
+          };
+          commit.verbose = true;
+          core = {
+            fsmonitor = true;
+            pager = lib.getExe pkgs.delta;
+            untrackedCache = true;
+          };
+          delta = {
+            side-by-side = false;
+            syntax-theme = "Dracula";
+          };
+          diff.algorithm = "histogram";
+          feature.manyFiles = true;
+          fetch.prune = true;
+          init.defaultBranch = "main";
+          interactive.diffFilter = "${lib.getExe pkgs.delta} --color-only";
+          merge.conflictstyle = "zdiff3";
+          pull.ff = "only";
+          push = {
+            autoSetupRemote = true;
+            default = "current";
+          };
+        };
+      };
+      lazygit.enable = true;
       neovim = {
         enable = true;
         package = inputs.neovim-nightly.packages.${system}.default;
