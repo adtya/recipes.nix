08d7ca34e72bdbe956d3e9d8b7b64f8732ada6e5
Adithya Nair <adtya@adtya.xyz>
2026-04-08 23:17:27 +0530

    amdgpu shenanigans

--- hosts/gloria/hardware/default.nix
+++ hosts/gloria/hardware/default.nix
@@ -5,6 +5,24 @@
   inputs,
   ...
 }:
+let
+  amdgpu-pm = pkgs.writeTextFile {
+    name = "amdgpu-pm-rules";
+    text = ''
+      SUBSYSTEM=="drm", DRIVERS=="amdgpu", ATTR{device/power_dpm_force_performance_level}="low"
+    '';
+    destination = "/etc/udev/rules.d/30-amdgpu-pm.rules";
+  };
+  mutter-preferred-primary-gpu = pkgs.writeTextFile {
+    name = "mutter-preferred-primary-gpu-rules";
+    text = ''
+      SUBSYSTEM=="drm", ENV{DEVTYPE}=="drm_minor", ENV{DEVNAME}=="/dev/dri/card[0-9]", SUBSYSTEMS=="pci", ATTRS{vendor}=="0x8086", ATTRS{device}=="0x3e9b", TAG+="mutter-device-preferred-primary"
+    '';
+    destination = "/etc/udev/rules.d/61-mutter-preferred-primary-gpu.rules";
+  };
+
+in
+
 {
   imports = [
     inputs.nixos-hardware.nixosModules.apple-t2
@@ -16,6 +34,11 @@
     enableIGPU = true;
     kernelChannel = "latest";
   };
+
+  services.udev.packages = [
+    amdgpu-pm
+    mutter-preferred-primary-gpu
+  ];
 
   boot = {
     initrd = {
@@ -33,6 +56,8 @@
       "i915"
       "dm-snapshot"
     ];
+
+    kernelParams = [ "amdgpu.dpm=0" ];
   };
 
   hardware = {
