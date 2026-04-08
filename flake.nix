0b38afd8a3e90f2e5d340fa92e3af2e2a519e48b
Adithya Nair <adtya@adtya.xyz>
2026-04-08 23:59:51 +0530

    remove nixConfig

--- flake.nix
+++ flake.nix
@@ -1,11 +1,4 @@
 {
-  nixConfig = {
-    extra-substituters = [ "https://nix-community.cachix.org" ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
   inputs = {
     disko.url = "github:nix-community/disko?ref=latest";
     jovian.url = "github:Jovian-Experiments/Jovian-NixOS?ref=development";
