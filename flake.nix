f03b7c4526251e3d144d1600b7be8d3a2b07d0a7
Adithya Nair <adtya@adtya.xyz>
2026-04-19 08:02:09 +0530

    darwin awards for the darwins

--- flake.nix
+++ flake.nix
@@ -23,8 +23,6 @@
       forAllSystems = lib.genAttrs [
         "x86_64-linux"
         "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
       ];
       pkgsFor =
         system:
