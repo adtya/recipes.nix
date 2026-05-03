faf9db71d09f707b49b82ac622c1929aeb312846
Adithya Nair <adtya@adtya.xyz>
2026-05-03 14:19:24 +0530

    fix treesitter grammers

--- nixvim/treesitter.nix
+++ nixvim/treesitter.nix
@@ -5,15 +5,24 @@
     highlight.enable = true;
     indent.enable = true;
     folding.enable = false;
+    grammarPackages = with config.plugins.treesitter.package.builtGrammars; [
+      bash
+      diff
+      dockerfile
+      git_config
+      git_rebase
+      gitattributes
+      gitcommit
+      gitignore
+      hyprlang
+      json
+      lua
+      markdown
+      markdown_inline
+      nix
+      regex
+      toml
+      yaml
+    ];
   };
-  grammarPackages = with config.plugins.treesitter.package.builtGrammars; [
-    bash
-    json
-    lua
-    markdown
-    nix
-    regex
-    toml
-    yaml
-  ];
 }
