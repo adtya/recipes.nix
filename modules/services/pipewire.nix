e67e3aa9c1ec30a488b3d1fd070723bf185436be
Adithya Nair <adtya@adtya.xyz>
2026-05-17 19:07:34 +0530

    low latency pipewire?

--- modules/services/pipewire.nix
+++ modules/services/pipewire.nix
@@ -1,4 +1,9 @@
-{ lib, config, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 let
   cfg = config.xyz.adtya.recipes.services.pipewire;
   preset-cfg = config.xyz.adtya.recipes.presets;
@@ -16,8 +21,37 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ pkgs.crosspipe ];
     services.pipewire = {
       enable = true;
+      extraConfig = {
+        pipewire."92-low-latency" = {
+          "context.properties" = {
+            "default.clock.rate" = 48000;
+            "default.clock.quantum" = 32;
+            "default.clock.min-quantum" = 32;
+            "default.clock.max-quantum" = 32;
+          };
+        };
+        pipewire-pulse."92-low-latency" = {
+          context.modules = [
+            {
+              name = "libpipewire-module-protocol-pulse";
+              args = {
+                pulse.min.req = "32/48000";
+                pulse.default.req = "32/48000";
+                pulse.max.req = "32/48000";
+                pulse.min.quantum = "32/48000";
+                pulse.max.quantum = "32/48000";
+              };
+            }
+          ];
+          stream.properties = {
+            node.latency = "32/48000";
+            resample.quality = 1;
+          };
+        };
+      };
       alsa = {
         enable = true;
         support32Bit = true;
